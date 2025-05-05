9054135eb00feba86e841b2b59ce57012887d8f7
lulipereira <lulipereira2006@gmail.com>
2025-05-05 08:41:16 -0300

    p-data, Luisana Pereira

--- src/assets/components/p-data.jsx
+++ src/assets/components/p-data.jsx
@@ -1,8 +1,9 @@
 const productosBase = [
-    { descripcion: 'Lapicera', precio: 15 },
-    { descripcion: 'Cuaderno', precio: 25 },
-    { descripcion: 'Mochila', precio: 50 },
-    { descripcion: 'Cartuchera', precio: 10 },
+    { descripcion: 'Auriculares', precio: 49000      },
+    { descripcion: 'Teclado', precio: 30000 },
+    { descripcion: 'Monitor', precio: 125000 },
+    { descripcion: 'Webcam', precio: 220000 },
+    { descripcion: 'Parlante Bluetooth', precio: 59000.9 },
   ];
   
   export default productosBase;
